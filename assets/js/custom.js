a9dccb684572b083503102b0b68fe5755b8e59ec
LoiTD <loitd@rikkeisoft.com>
2022-10-20 00:34:15 +0700

    up

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,11 @@
-
-import data from '../json/data.json' assert {type: 'json'};
-var main = (function ($) {
-    // once the page is loaded go to index2.html
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-    window.onload = function () {
-        const divs = $("#thumbnails article").get().sort(function () {
-            return Math.round(Math.random()) - 1; //random so we get the right +/- combo
-        }).slice(0, 23);
-        $(divs).appendTo(divs[0].parentNode).show();
-        if (!isMobile) {
-            setTimeout(() => {
-                $("#thumbnails article .thumbnail").first()[0].click()
-            }, 500);
-        }
-    }
-})(jQuery); 
+// once the page is loaded go to index2.html
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+const divs = $("#thumbnails article").get().sort(function () {
+    return Math.round(Math.random()) - 0.5; //random so we get the right +/- combo
+}).slice(0, 23);
+$(divs).appendTo(divs[0].parentNode).show();
+if (!isMobile) {
+    setTimeout(() => {
+        $("#thumbnails article .thumbnail").first()[0].click()
+    }, 500);
+}
